06fd8162747552e70d93353b049b77bbe4e17a30
Justin <71615891+whitebread778@users.noreply.github.com>
2022-03-12 14:10:29 -0800

    project set up

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery, LocationOutlinedIcon } from '@material-ui/core';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
+const Map = () => {
+    return (
+        <h1>Map</h1>
+    );
+}
+
+export default Map;
